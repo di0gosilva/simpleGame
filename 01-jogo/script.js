99346bc4958e69ca10f5bd573b8164166384637d
DIOGO DA SILVA <diogo_silva8@estudante.sesisenai.org.br>
2025-02-20 16:23:31 -0300

    continuação do jogo

--- 01-jogo/script.js
+++ 01-jogo/script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("jogoCanva")
 const context = canvas.getContext("2d")
+let gameOver = false
+let gravidade = 0.6
 
 document.addEventListener("click", (e) => {
     if (gameOver) {
@@ -7,17 +9,25 @@ document.addEventListener("click", (e) => {
     } 
 })
 
-let gameOver = false
+document.addEventListener("keypress", (e) => {
+    if(e.code == "Space" && personagem.pulando == false) {
+        personagem.velocidadey = -15
+        personagem.pulando = true
+    }
+})     
 
 const personagem = {
-    posicaox: 35,
-    posicaoy: canvas.height - 15,
-    width: 15,
-    height: 15,
+    posicaox: 100,
+    posicaoy: canvas.height - 20,
+    width: 20,
+    height: 20,
+    velocidadey: 0,
+    pulando: false,
+    noChao: true
 }
 
 function desenhaPersonagem() {
-    context.fillStyle = "lightgreen"
+    context.fillStyle = "yellow"
     context.fillRect(
         personagem.posicaox, 
         personagem.posicaoy, 
@@ -26,16 +36,29 @@ function desenhaPersonagem() {
     )
 }
 
+function atualizaPersonagem() {
+    if(personagem.pulando) {
+        personagem.velocidadey += gravidade 
+        personagem.posicaoy += personagem.velocidadey
+
+        if (personagem.posicaoy >= canvas.height - 20) {
+            personagem.posicaoy = canvas.height - 20
+            personagem.velocidadey = 0
+            personagem.pulando = false
+        }
+    }
+}
+
 const obstaculo = {
     posicaox: canvas.width - 50,
     posicaoy: canvas.height - 100,
-    width: 15,
+    width: 20,
     height: 100,
-    velocidade: 5,
+    velocidade: 2,
 }
 
 function desenhaObstaculo() {
-    context.fillStyle = "green"
+    context.fillStyle = "red"
     context.fillRect(
         obstaculo.posicaox, 
         obstaculo.posicaoy, 
@@ -46,20 +69,30 @@ function desenhaObstaculo() {
 
 function atualizaObstaculo() {
     obstaculo.posicaox -= obstaculo.velocidade
+    if (obstaculo.posicaox <= 0 - obstaculo.width) {
+        let altura_random = (Math.random() * 50) + 90
+        obstaculo.posicaox = canvas.width - 50
+        obstaculo.height = altura_random
+        obstaculo.posicaoy = canvas.height - altura_random
+        obstaculo.velocidade += 0.5
+    }
 }
 
 function verificaColisao() {
     if(
-        personagem.posicaox < obstaculo.posicaox + obstaculo.width && personagem.posicaox + personagem.width +2 > obstaculo.posicaox
+        personagem.posicaox < obstaculo.posicaox + obstaculo.width &&
+        personagem.posicaox + personagem.width > obstaculo.posicaox &&
+        personagem.posicaoy < obstaculo.posicaoy + obstaculo.height &&
+        personagem.posicaoy + personagem.height > obstaculo.posicaoy
     ) {
-        colidiu()
-    }
+        houveColisao()
+    } 
 }
 
-function colidiu() {
+function houveColisao() {
     obstaculo.velocidade = 0
     atualizaObstaculo()
-    context.fillStyle = 'lightgreen'
+    context.fillStyle = 'red'
     context.fillRect((canvas.width/2)-100, (canvas.height/2)-50, 200, 100)
     context.fillStyle = "black"
     context.font = "25px Arial"
@@ -69,11 +102,11 @@ function colidiu() {
 
 function loop() {    
     context.clearRect(0, 0, canvas.width, canvas.height)
-    desenhaObstaculo()
     atualizaObstaculo()
-    desenhaPersonagem()
+    desenhaObstaculo()
+    atualizaPersonagem()
+    desenhaPersonagem() 
     verificaColisao()
-    console.log(obstaculo.posicaox)
     requestAnimationFrame(loop)
 }
 
